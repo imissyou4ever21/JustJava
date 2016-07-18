b91969fc04d3c0e84f9c225afa7bbbb1aa0436f4
imissyou4ever21 <imissyou4ever21@gmail.com>
2016-07-18 11:59:18 +0800

    add ScrollView, Checkbox, input Name

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import java.text.NumberFormat;
 
@@ -15,6 +17,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calculatePrice(quantity);
+
     }
 
     /**
@@ -22,29 +26,41 @@ public class MainActivity extends ActionBarActivity {
      */
     public void submitOrder(View view) {
         int price = quantity * 5;
-        String priceMessage = "Price $" + price;
-        priceMessage = priceMessage + " \nThank you:)";
-        display(quantity);
+        CheckBox wippeCreamCheckBox = (CheckBox)findViewById(R.id.Wipped_cream_checkbox);
+        boolean hasWippedCream = wippeCreamCheckBox.isChecked();
+        CheckBox chochlateBox = (CheckBox)findViewById(R.id.Chocolate_checkbox);
+        boolean hasChocolate =  chochlateBox.isChecked();
+        String priceMessage = creatOrderSummary(price, hasWippedCream, hasChocolate);
         displayMessage(priceMessage);
     }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param quantity is the number of cups of coffee ordered
+     */
+    private void calculatePrice(int quantity) {
+        int price = quantity * 5;
+    }
+
     /**
     * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
         quantity = quantity+1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
         quantity = quantity-1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -63,5 +79,16 @@ public class MainActivity extends ActionBarActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
+    }
+
+    private String creatOrderSummary(int price, boolean addCream, boolean addChocolate)
+    {
+        String priceMessage= "Name: Henry Cheng";
+        priceMessage += "\nAdd cream? " + addCream;
+        priceMessage += "\nAdd cream? " + addChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank you!";
+        return priceMessage;
     }
 }
